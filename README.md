c448112d0084847b208b5171c7eb2197e7b79bbd
wanderson648 <testepedroca@gmail.com>
2023-11-21 18:00:11 -0300

    configura e inicia projeto

---
+++ README.md
@@ -0,0 +1,1 @@
+# Desafio-backend, Attornatus.
---
+++ src/test/java/com/wo/desafioattornatusapi/DesafioAttornatusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wo.desafioattornatusapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioAttornatusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
